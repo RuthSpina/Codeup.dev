612ed8dc483ee060feccba03681eecc82c03c96e
Ruth Spina <ruthspina@Ruths-MacBook-Air.local>
2014-09-20 19:35:54 -0500

    'php doc to add a table within mysql' 'deleted' 'deleted'

--- public/create_a_table.php
+++ public/create_a_table.php
@@ -2,34 +2,34 @@
 
 require 'dbconnection.php';
 
-// // Create the query and assign to var
-// $query = 'CREATE TABLE national_parks (
-//     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-//     email VARCHAR(240) NOT NULL,
-//     name VARCHAR(50) NOT NULL,
-//     PRIMARY KEY (id)
-// )';
+// query and assign to var
+$query = 'CREATE TABLE national_parks (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    email VARCHAR(240) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+)';
 
-// Run query, if there are errors they will be thrown as PDOExceptions
-//$dbc->exec($query);
+Run query, if there are errors they will be thrown as PDOExceptions
+$dbc->exec($query);
 
-// Create INSERT query
-// $query = "INSERT INTO users (email, name) VALUES ('Isela@email.com', 'Isela Spina')";
+Create INSERT query
+$query = "INSERT INTO users (email, name) VALUES ('Isela@email.com', 'Isela Spina')";
 
-// // Execute Query
-// $numRows = $dbc->exec($query);
-// echo "Inserted $numRows row." . PHP_EOL;
+// Execute Query
+$numRows = $dbc->exec($query);
+echo "Inserted $numRows row." . PHP_EOL;
 
-// $users = [
-//     ['email' => 'jason@codeup.com',   'name' => 'Jason Straughan'],
-//     ['email' => 'chris@codeup.com',   'name' => 'Chris Turner'],
-//     ['email' => 'michael@codeup.com', 'name' => 'Michael Girdley']
-// ];
+$users = [
+    ['email' => 'jason@codeup.com',   'name' => 'Jason Straughan'],
+    ['email' => 'chris@codeup.com',   'name' => 'Chris Turner'],
+    ['email' => 'michael@codeup.com', 'name' => 'Michael Girdley']
+];
 
-// foreach ($users as $user) {
-//     $query = "INSERT INTO users (email, name) VALUES ('{$user['email']}', '{$user['name']}')";
+foreach ($users as $user) {
+    $query = "INSERT INTO users (email, name) VALUES ('{$user['email']}', '{$user['name']}')";
 
-//     $dbc->exec($query);
+    $dbc->exec($query);
 
-//     echo "Inserted ID: " . $dbc->lastInsertId() . "<br>";
-// }
+    echo "Inserted ID: " . $dbc->lastInsertId() . "<br>";
+}
